ba6d32fc262c529d89ff799a05cd4ec636688d62
Luis Araujo <laraujo@e-goi.com>
2025-11-04 10:58:05 +0000

    improv/BB-27863: improve logger

--- controllers/admin/ProductsController.php
+++ controllers/admin/ProductsController.php
@@ -261,16 +261,16 @@ class ProductsController extends SmartMarketingBaseController
 
             if ($syncVariations) {
                 DebugLogger::log(
-                    "[EGOI-PS17]::" . __FUNCTION__ . "::LOG: START UPGRADE TO 3.1.1 . " . json_encode($syncVariations)
+                    "[EGOI-PS8]::" . __FUNCTION__ . "::LOG: START UPGRADE TO 3.1.1 . " . json_encode($syncVariations)
                 );
                 $prodId = is_array($product) ? (int)$product['id_product'] : (int)$product->id;
                 $ipaList = Product::getProductAttributesIds($prodId);
 
                 DebugLogger::log(
-                    "[EGOI-PS17]::" . __FUNCTION__ . "::LOG: START UPGRADE TO 3.1.1 . " . json_encode($prodId)
+                    "[EGOI-PS8]::" . __FUNCTION__ . "::LOG: START UPGRADE TO 3.1.1 . " . json_encode($prodId)
                 );
                 DebugLogger::log(
-                    "[EGOI-PS17]::" . __FUNCTION__ . "::LOG: START UPGRADE TO 3.1.1 . " . json_encode($ipaList)
+                    "[EGOI-PS8]::" . __FUNCTION__ . "::LOG: START UPGRADE TO 3.1.1 . " . json_encode($ipaList)
                 );
                 if (!empty($ipaList)) {
                     foreach ($ipaList as $ipaRow) {
